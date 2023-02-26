3d1b559678518f63cbc159daeb752c567f990ae0
Jon <104590083+cirillojon@users.noreply.github.com>
2023-02-25 21:08:37 -0500

    Time: 7 ms (5.52%), Space: 42.4 MB (14.10%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -7,20 +7,16 @@ class Solution {
         StringBuilder s = new StringBuilder(); // Create a new StringBuilder object called s
 
         for(int i = 0; i < digits.length; i++) // Loop through each digit in the input array
-        {
             s.append(digits[i]); // Append the current digit to the StringBuilder object s
-        }
-
-
+        
         BigInteger num = new BigInteger(s.toString()); // Create a new BigInteger object called num by parsing the contents of the StringBuilder object s as a string
         num = num.add(BigInteger.ONE); // Add one to the BigInteger object num
-
 
         int[] res = new int[(String.valueOf(num)).length()]; // Create a new integer array called res with a length equal to the length of the string representation of the BigInteger object num
 
 
         for(int i = 0; i < res.length; i++) // Loop through each element of the res array
-            res[i] = Integer.parseInt(    String.valueOf((String.valueOf(num)).charAt(i))  , 10 ) ; // Parse the character at the current index of the string representation of the BigInteger object num as an integer and store it in the current element of the res array
+            res[i] = Integer.parseInt(String.valueOf((String.valueOf(num)).charAt(i)), 10) ; // Parse the character at the current index of the string representation of the BigInteger object num as an integer and store it in the current element of the res array
         
         return res; // Return the res array
     }
