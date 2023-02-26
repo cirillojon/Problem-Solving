717e881166b6ce37b76ec4d4420b07b3044675de
Jon <104590083+cirillojon@users.noreply.github.com>
2023-02-25 21:07:35 -0500

    Time: 7 ms (5.52%), Space: 42.6 MB (10.19%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,26 +1,27 @@
-import java.math.BigInteger; 
-class Solution {
-    public int[] plusOne(int[] digits) {
-        int val = 0;
-        
-        StringBuilder s = new StringBuilder();
+import java.math.BigInteger; // Import the BigInteger class from the Java math library
 
-        for(int i = 0; i < digits.length; i++)
+class Solution {
+    public int[] plusOne(int[] digits) { // Define a method called plusOne that takes an array of integers as input and returns an array of integers as output
+        int val = 0; // Initialize an integer variable called val with a value of 0
+
+        StringBuilder s = new StringBuilder(); // Create a new StringBuilder object called s
+
+        for(int i = 0; i < digits.length; i++) // Loop through each digit in the input array
         {
-            s.append(digits[i]);
+            s.append(digits[i]); // Append the current digit to the StringBuilder object s
         }
 
-        //System.out.println(s.toString());
-        BigInteger num = new BigInteger(s.toString());
-        num = num.add(BigInteger.ONE);
 
-        //System.out.println(num.toString());
-        int[] res = new int[(String.valueOf(num)).length()];
-        //System.out.println(res.length);
+        BigInteger num = new BigInteger(s.toString()); // Create a new BigInteger object called num by parsing the contents of the StringBuilder object s as a string
+        num = num.add(BigInteger.ONE); // Add one to the BigInteger object num
 
-        for(int i = 0; i < res.length; i++)
-            res[i] = Integer.parseInt(    String.valueOf((String.valueOf(num)).charAt(i))  , 10 ) ;
+
+        int[] res = new int[(String.valueOf(num)).length()]; // Create a new integer array called res with a length equal to the length of the string representation of the BigInteger object num
+
+
+        for(int i = 0; i < res.length; i++) // Loop through each element of the res array
+            res[i] = Integer.parseInt(    String.valueOf((String.valueOf(num)).charAt(i))  , 10 ) ; // Parse the character at the current index of the string representation of the BigInteger object num as an integer and store it in the current element of the res array
         
-        return res;
+        return res; // Return the res array
     }
 }
