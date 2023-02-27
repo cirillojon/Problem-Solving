c574a0065dad39d3d15721245cfa5e08620c01f4
Jon <104590083+cirillojon@users.noreply.github.com>
2023-02-26 19:29:19 -0500

    Time: 1 ms (74.33%), Space: 40.6 MB (39.75%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,13 +4,16 @@ class Solution {
         int min = Integer.MAX_VALUE; int mindex = 0;
         for(int i = 0; i < strs.length; i++)
             if(strs[i].length() < min){
-               min = strs[i].length(); mindex = i;}
+                    min = strs[i].length();
+                    mindex = i;
+                }
         for(int i = 0; i < min; i++){
             ch = strs[mindex].charAt(index);
             for(int j = 0; j < strs.length; j++)
                 if(ch != strs[j].charAt(index)) return res.toString();
             res.append(ch);
-            index++;}
+            index++;
+        }
         return res.toString();
     }
 }   
