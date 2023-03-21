1881ba718680bb3176bfadee830a921d8166f8c3
Jon <104590083+cirillojon@users.noreply.github.com>
2023-03-21 12:47:46 -0400

    Time: 4 ms (57.99%), Space: 60.3 MB (20.69%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -5,7 +5,9 @@ class Solution {
         for(int i = 0; i < nums.length; i++)
         {
             if(nums[i] == 0 && bool == true)
-                res += ++count;
+            {
+                count++; res += count;
+            }
             
             if(nums[i] == 0 && bool == false)
             {            
