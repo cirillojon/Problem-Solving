9795116832edeeb6303a7368b2333bc775fd4855
Jon <104590083+cirillojon@users.noreply.github.com>
2023-04-11 18:03:15 -0400

    Time: 1124 ms (5.02%), Space: 57.8 MB (26.00%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.java
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.java
@@ -18,7 +18,6 @@ class Solution {
              rightMax = rightTemp;
         }
 
-
         if(leftMax == Integer.MIN_VALUE && rightMax == Integer.MIN_VALUE)
         return 0;
 
@@ -28,28 +27,25 @@ class Solution {
         return Math.max(leftMax,rightMax);
     }
 
-// This method implements the Sieve of Eratosthenes algorithm to find all prime numbers up to a given integer n.
-public boolean[] sieve(int n) {
-    // Create a boolean array of size n+1 to represent whether each integer from 0 to n is prime.
-    boolean prime[] = new boolean[n + 1];
+    // Implements the Sieve of Eratosthenes algorithm
+    public boolean[] sieve(int n) {
+        // Creates a boolean array to contain whether or not an int 0-n is prime
+        boolean prime[] = new boolean[n + 1];
 
-    // Initialize all elements of the array to true, since initially we assume all integers are prime.
-    for (int i = 0; i <= n; i++) {
-        prime[i] = true;
-    }
+        // Initialize all elements of the array to true
+        for (int i = 0; i <= n; i++) {
+            prime[i] = true;
+        }
 
-    // Loop through all prime numbers up to the square root of n.
-    for (int p = 2; p * p <= n; p++) {
-        // If p is prime (i.e., prime[p] is true), then mark all multiples of p as composite (i.e., prime[p*i] = false)
-        if (prime[p] == true) {
-            for (int i = p * p; i <= n; i += p) {
-                prime[i] = false;
+        // Loop through all prime numbers up to the square root of n
+        for (int p = 2; p * p <= n; p++) {
+            // If p is prime, then mark all multiples of p as false 
+            if (prime[p] == true) {
+                for (int i = p * p; i <= n; i += p) {
+                    prime[i] = false;
+                }
             }
         }
+        return prime;
     }
-
-    // Return the boolean array, where each element represents whether the corresponding integer is prime.
-    return prime;
-}
-
 }
