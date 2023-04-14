155124954050f4b31464fa79e08f819e86b502ce
Jon <104590083+cirillojon@users.noreply.github.com>
2023-04-14 14:34:33 -0400

    Time: 2 ms (68.05%), Space: 40.4 MB (92.10%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,14 +1,11 @@
 class Solution {
     public char findTheDifference(String s, String t) {
         int totalS = 0; int totalT = 0;
-
         for(int i = 0; i < t.length(); i++){   
             if(i < s.length())
             totalS = totalS + (int)(s.charAt(i));
             totalT = totalT + (int)(t.charAt(i));
         }
-    
         return (char)(totalT-totalS);
-        
     }
 }
