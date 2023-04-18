195d2f274931d41e6402982a9474332910f72264
Jon <104590083+cirillojon@users.noreply.github.com>
2023-04-18 17:03:27 -0400

    Time: 58 ms (37.30%), Space: 101.3 MB (7.21%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.java
@@ -3,9 +3,7 @@ class Solution {
         
         HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
         for(int i = 0; i < nums.length; i++) map.put(nums[i],i);
-        
-        for(int i = 0; i < operations.length; i++)
-        {
+        for(int i = 0; i < operations.length; i++){
             nums[map.get(operations[i][0])] = operations[i][1];
             map.put(operations[i][1], map.get(operations[i][0]));
         }
