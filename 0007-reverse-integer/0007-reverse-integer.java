74f32869ae1792c1a4a6a9dff1626827e9c45e96
Jon <104590083+cirillojon@users.noreply.github.com>
2023-04-21 18:35:37 -0400

    Time: 4 ms (9.62%), Space: 40.8 MB (10.23%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        
+        Stack<Integer> s = new Stack<Integer>();
+        boolean neg = false; String str = String.valueOf(x);
+        if(str.length() == 1) return x; int res = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '-') { neg = true; continue;}
+            s.push(Character.getNumericValue(str.charAt(i)));
+        }   while(s.peek() == 0) s.pop();
+        StringBuilder sb = new StringBuilder();
+        if(neg == true) sb.append("-");
+        while(!s.isEmpty()) sb.append(s.pop());
+        try{ res = Integer.valueOf(sb.toString()); }
+        catch(NumberFormatException e){ return 0; } return res;
+    }
+}
