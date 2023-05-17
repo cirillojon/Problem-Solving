4fb3012c6a2c7236d668531e5790e57c7061ef73
Jon <104590083+cirillojon@users.noreply.github.com>
2023-05-17 11:06:46 -0400

    Time: 5 ms (89.63%), Space: 65.3 MB (25.69%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -3,6 +3,6 @@ class Solution {
         int arr[] = new int[100000]; int index = 0; int res = Integer.MIN_VALUE;
         while(head != null){arr[index++] = head.val; head = head.next;}
         for(int i = 0 ; i < index/2; i++) if(arr[i] + arr[index-i-1] > res) 
-        res = arr[i] + arr[index-i-1]; return res;
-}
+        res = arr[i] + arr[index-i-1]; return res; 
+    }
 }
