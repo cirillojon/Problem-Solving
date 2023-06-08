522e7fbada7794e899839b66da8bb6c6be990f27
Jon <104590083+cirillojon@users.noreply.github.com>
2023-06-08 11:20:33 -0400

    Time: 116 ms (8.55%), Space: 45.6 MB (7.88%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,20 +1,36 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        
+        // Initialize variables
         int[] res = new int[nums.length];
-        int index = 0; boolean flag = false; int count = 0;
+        int index = 0; 
+        int count = 0;
+        boolean flag = false; 
+        
+        // Loop through each element
         for(int i = 0; i < nums.length; i++){
-            index = i + 1;
+            index = i + 1; // Search index always starts at element directly after
+            
+            // Loop circularly until either we have searched all numbers (except start) or found larger 
             while(count != nums.length-1 && flag == false){
-                if(index == nums.length) index = 0;
+                if(index == nums.length) index = 0; // Wrap around to start
+                
+                // If larger number is found, assign it to res[i]
                 if(nums[index] > nums[i]){
                     res[i] = nums[index];
                     flag = true;
-                }
+                } 
+                
+                // Iterate values
                 index++; count++;
             }
-            if(count == nums.length || flag == false) res[i] = -1;
-            count = 0; flag = false;
             
+
+            if(flag == false) 
+                res[i] = -1;
+            
+            count = 0; 
+            flag = false;
         }
         return res;
     }
