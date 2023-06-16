bfc0d159221936303dafc98a605889d8d3426ea0
Jon <104590083+cirillojon@users.noreply.github.com>
2023-06-16 10:17:31 -0400

    Update 0030-substring-with-concatenation-of-all-words.java

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,26 +1,10 @@
 class Solution {
-    public List<Integer> findSubstring(String s, String[] words) {
-        List<Integer> res = new ArrayList<>();
-        Map<String, Integer> cache = new HashMap<>();
-        
-        // Create map filled with words and their count
-        for(int i = 0; i < words.length; i++) 
-            cache.merge(words[i], 1, Integer::sum);
-
-        // Length of 'Substring with Concatenation of All Words'
-        int len = words[0].length()*words.length;
-       
-        for(int i = 0; i <= s.length() - len; i++)
-            // Compare map for given substring with master cache
-            if(is_valid(s.substring(i, len + i), words, cache))
-                // If equal, add index to res
-                res.add(i); return res;
-    }
-    public boolean is_valid(String s, String[] words, Map<String, Integer> cache){
-         Map<String, Integer> map = new HashMap<>();
-         int len = words[0].length(); int limit = s.length() - len;
-         for(int i = 0; i <= limit; i += len)
-            map.merge(s.substring(i, len + i), 1, Integer::sum);
-       return map.equals(cache);
-    }
+    public List<Integer> findSubstring(String s, String[] w) {
+      List<Integer> r = new ArrayList<>(); Map<String, Integer> m = new HashMap<>();
+      for(int i = 0; i < w.length; i++) m.merge(w[i], 1, Integer::sum); 
+      int len = w[0].length()*w.length; for(int i = 0; i <= s.length() - len; i++) 
+      if(is_valid_word_map(s.substring(i, len + i), w[0].length(), m)) r.add(i); return r; } 
+    public boolean is_valid_word_map(String s, int l, Map<String, Integer> m) {
+      Map<String, Integer> t = new HashMap<>(); for(int i = 0; i <= s.length() - l; i += l) 
+      t.merge(s.substring(i, l + i), 1, Integer::sum); return t.equals(m); } 
 }
