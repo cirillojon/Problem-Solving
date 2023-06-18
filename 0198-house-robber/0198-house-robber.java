dce9741633342ae7bcdd91a42e806c7320545062
Jon <104590083+cirillojon@users.noreply.github.com>
2023-06-18 09:21:35 -0400

    Time: 1 ms (100.00%), Space: 39.7 MB (96.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,11 +1,12 @@
 class Solution{
     public int rob(int[] nums){
-        int res = 0; Map<Integer, Integer> cache = new HashMap<>();
-        res = helper(nums, cache, res, nums.length - 1); return res; }
-    public int helper(int[] nums, Map<Integer, Integer> cache, int res, int index){
-        if(index == 0) return nums[0]; if(index == 1) return Math.max(nums[0], nums[1]);
-        if(cache.containsKey(index)) return cache.get(index);
-        res  = Math.max(nums[index] + helper(nums, cache, res, index - 2), helper(nums, cache, res, index - 1));
-        cache.put(index, res); return res;
+        int r = 0; Map<Integer, Integer> m = new HashMap<>();
+        r = helper(nums, m, r, nums.length-1); return r; }
+    public int helper(int[] nums, Map<Integer, Integer> m, int r, int i){
+        if(i == 0) return nums[0];
+        if(i == 1) return Math.max(nums[0], nums[1]);
+        if(m.containsKey(i)) return m.get(i);
+        r = Math.max(nums[i] + helper(nums, m, r, i -2), helper(nums, m, r, i - 1));
+        m.put(i, r); return r;
     }
 }
