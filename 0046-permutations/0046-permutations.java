c25cb181162f0a934b9b5bba86be6a58a68f5abf
Jon <104590083+cirillojon@users.noreply.github.com>
2023-06-18 09:15:06 -0400

    Time: 1 ms (98.87%), Space: 44 MB (33.02%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,21 +1,22 @@
 class Solution{
-    public List<List<Integer>> permute(int[] nums){
+    public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        if(nums == null || nums.length == 0) return res;
+        if(nums.length == 0 || nums == null) return res;
         helper(res, nums, new ArrayList<>(), new boolean[nums.length]);
         return res;
     }
     public void helper(List<List<Integer>> res, int[] nums, List<Integer> list, boolean[] used){
         if(list.size() == nums.length){
-            res.add(new ArrayList<>(list));
-            return;
+            res.add(new ArrayList<>(list)); return;
         }
+        
         for(int i = 0; i < nums.length; i++){
             if(used[i] == true) continue;
             list.add(nums[i]); used[i] = true;
             helper(res, nums, list, used);
             list.remove(list.size() - 1); used[i] = false;
         }
+        
         return;
     }
 }
