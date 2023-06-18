f708a162d8b0ef8e06ed45df88d425b0d1501418
Jon <104590083+cirillojon@users.noreply.github.com>
2023-06-18 08:50:26 -0400

    Time: 1 ms (98.87%), Space: 44.2 MB (6.93%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,39 +1,21 @@
-class Solution { 
-    // Method to get all permutations of the input array nums
-    public List<List<Integer>> permute(int[] nums) { 
-        // Initialize an empty list to hold all permutations
+class Solution{
+    public List<List<Integer>> permute(int[] nums){
         List<List<Integer>> res = new ArrayList<>();
-        // If input array nums is null or its length is 0, return the empty list
-        if(nums.length == 0 || nums == null) return res;
-        // Call the helper method with initial parameters. It starts the recursive process of finding all permutations.
+        if(nums == null || nums.length == 0) return res;
         helper(res, nums, new ArrayList<>(), new boolean[nums.length]);
-        // Return the list of all permutations 
         return res;
     }
-
-    // Recursive helper method to find all permutations
     public void helper(List<List<Integer>> res, int[] nums, List<Integer> list, boolean[] used){
-        
-        // If the size of the current list equals the length of the input array, a permutation is found.
-       if(list.size() == nums.length){
-           res.add(new ArrayList<>(list));
-           return;
-       }
-        
-        // Loop through the input array
-        for(int i = 0; i < nums.length; i++) {
-            // If the current number is already used in this branch of recursion, skip to the next iteration
-            if(used[i] == true) continue;
-            // Add the current number to the list and mark it as used
-            list.add(nums[i]); used[i] = true;
-            // Continue the recursion with the updated list and used array
-            helper(res, nums, list, used);
-            // After the recursion, remove the last number from the list and mark it as unused.
-            list.remove(list.size() - 1); used[i] = false;
-            // This is backtracking - it undoes the changes for the next branch of recursion.
-            
+        if(list.size() == nums.length){
+            res.add(new ArrayList<>(list));
+            return;
         }
-        // End the current recursive call
+        for(int i = 0; i < nums.length; i++){
+            if(used[i] == true) continue;
+            list.add(nums[i]); used[i] = true;
+            helper(res, nums, list, used);
+            list.remove(list.size() - 1); used[i] = false;
+        }
         return;
     }
 }
