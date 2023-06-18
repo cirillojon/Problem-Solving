da11844dc5dfe2cf2ad8e030497ddf2e6d02be0e
Jon <104590083+cirillojon@users.noreply.github.com>
2023-06-18 18:54:32 -0400

    Time: 74 ms (33.40%), Space: 116.2 MB (68.07%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,13 +1,13 @@
 class LRUCache {
-    int capacity = 0;
-    Map<Integer, Node> map = new HashMap<>();  
+    Map<Integer, Node> map = new HashMap<>();
+    int cap = 0;
     Node head = new Node(0,0);
     Node tail = new Node(0,0);
-    
-    public LRUCache(int _capacity) {
-      head.next = tail;
-      tail.prev = head;
-      capacity = _capacity;
+
+    public LRUCache(int capacity) {
+        cap = capacity;
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
@@ -16,38 +16,31 @@ class LRUCache {
             remove(node);
             insert(node);
             return node.val;
-        }
-        return -1;
+        } return -1;
     }
     
     public void put(int key, int value) {
-         if(map.containsKey(key)){
-            remove(map.get(key));
-         }
-        if(map.size() == capacity){
-            remove(tail.prev);
-        }
-        
-        insert(new Node(key, value));
-        
-    }
-    public void insert(Node node){
-        map.put(node.key, node);
-        node.next = head.next;
-        node.next.prev = node;
-        head.next = node; 
-        node.prev = head;
+        if(map.containsKey(key)) remove(map.get(key));
+        if(map.size() == cap) remove(tail.prev);
+        insert(new Node(key,value));
     }
     public void remove(Node node){
         map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
+    public void insert(Node node){
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
     
-class Node{
-    Node prev, next; 
-    int key, val; 
-    Node(int _key, int _val){
+    class Node{
+        Node prev, next;
+        int key, val;
+        Node(int _key, int _val){
             key = _key;
             val = _val;
         }
