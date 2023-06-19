cd900e6728cdaae38711512364d4ea49dd9c7940
Jon <104590083+cirillojon@users.noreply.github.com>
2023-06-19 15:02:10 -0400

    Time: 20 ms (89.29%), Space: 57.5 MB (5.36%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,8 +3,7 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++){
             if(map.containsKey(nums[i]) && (Math.abs(map.get(nums[i]) - i) <= k))
-                return true; 
-            map.put(nums[i], i);
+                return true; map.put(nums[i], i);
         } return false;
     }
 }
