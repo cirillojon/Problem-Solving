dd7598554dfe32786c6ec707a418208fa2d69558
Jon <104590083+cirillojon@users.noreply.github.com>
2023-07-30 19:15:08 -0700

    Time: 59 ms (87.70%), Space: 16.5 MB (93.15%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,8 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         
+        r_chars = {}
+        m_chars = {}
+        
         for c in ransomNote:
-            if ransomNote.count(c) <= magazine.count(c):
+            r_chars[c] = r_chars.get(c, 0) + 1
+        for c in magazine:
+            m_chars[c] = m_chars.get(c, 0) + 1
+        for c in ransomNote:
+            if c in r_chars and c not in m_chars:
+                return False
+            if r_chars[c] <= m_chars[c]:
                 continue
             else:
                 return False
