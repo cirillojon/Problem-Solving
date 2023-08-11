91d1e24c9a9371380a7a90e8df3879dde017e403
Jon <104590083+cirillojon@users.noreply.github.com>
2023-08-11 09:17:41 -0400

    Time: 46 ms (53.50%), Space: 16.3 MB (85.61%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,17 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         
         stack = []
+        
         for st in s:
             if st == '(':
                 stack.append(')')
-
-            elif st == '{':
-                stack.append('}')
-            
             elif st == '[':
                 stack.append(']')
-
+            elif st == '{':
+                stack.append('}')
+                
             elif len(stack) == 0 or stack.pop() != st:
                 return False
-
+            
         return True if len(stack) == 0 else False
