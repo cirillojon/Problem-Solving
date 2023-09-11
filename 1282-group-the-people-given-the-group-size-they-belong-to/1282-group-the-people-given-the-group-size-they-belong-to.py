815bdc3217b165a2b45b61831f9c05288692b130
Jon <104590083+cirillojon@users.noreply.github.com>
2023-09-11 14:09:30 -0400

    Time: 87 ms (25.78%), Space: 16.5 MB (64.84%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        res = []
+        map = {}
+        for i, num in enumerate(groupSizes):
+            if num not in map:
+                map[num] = [i]
+            else:
+                map[num].append(i)
+        
+        for k, v in map.items():
+            if len(map[k]) == k:
+                res.append(map[k])
+            else:
+                for i in range(0, len(map[k]), k):
+                    res.append(map[k][i:i+k])
+        return sorted(res, key = lambda x : len(x))
