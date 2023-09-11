21afb8cfd05666bac1bba2816966d2abe6f1e9aa
Jon <104590083+cirillojon@users.noreply.github.com>
2023-09-11 14:11:50 -0400

    Time: 74 ms (66.63%), Space: 16.4 MB (91.52%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -1,17 +1,17 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         res = []
-        map = {}
-        for i, num in enumerate(groupSizes):
-            if num not in map:
-                map[num] = [i]
+        size_map = {}
+        for i, size in enumerate(groupSizes):
+            if size not in size_map:
+                size_map[size] = [i]
             else:
-                map[num].append(i)
+                size_map[size].append(i)
         
-        for k, v in map.items():
-            if len(map[k]) == k:
-                res.append(map[k])
+        for size, size_list in size_map.items():
+            if len(size_map[size]) == size:
+                res.append(size_map[size])
             else:
-                for i in range(0, len(map[k]), k):
-                    res.append(map[k][i:i+k])
+                for i in range(0, len(size_map[size]), size):
+                    res.append(size_map[size][i:i+size])
         return res
