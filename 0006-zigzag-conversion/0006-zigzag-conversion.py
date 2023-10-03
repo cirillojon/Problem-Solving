854bbe204d1f0e7f18b7e63a3651e9bbc783a588
Jon <104590083+cirillojon@users.noreply.github.com>
2023-10-02 22:35:45 -0400

    Time: 59 ms (62.14%), Space: 16.5 MB (26.05%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,35 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+
+        # ex) s='ABC', numRows=1 => ABC
+        if numRows == 1:
+            return s
+        
+        # Create list for each row
+        row_list = [[] for i in range(numRows)]
+
+        # Initialize counters
+        count = 0
+        col_count = 0
+
+        # Loop through all letters in s
+        while count < len(s):
+            # Add each full column
+            if col_count == 0 or col_count == numRows-1:
+                for i in range(numRows):
+                    # Add letter to corresponding row_list
+                    row_list[i].append(s[count])
+                    count += 1
+                    if count == len(s):
+                        break
+                col_count = 0
+            # Add one letter per zigzag columb
+            # 'numRows-col_count-1' ~ bottom->up
+            else:
+                row_list[numRows-col_count-1].append(s[count])
+                count +=1
+                
+            col_count += 1
+
+        return(''.join([''.join(r) for r in row_list]))
+        
