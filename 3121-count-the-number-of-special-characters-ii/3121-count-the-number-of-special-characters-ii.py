182d2b3e264c0d01a5b7b0eb9758d963a4705680
Jon <104590083+cirillojon@users.noreply.github.com>
2026-05-26 19:55:38 -0700

    Time: 949 ms (5.3%), Space: 21.6 MB (47.06%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,15 +1,14 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
-        first_indices = {}
+        indices = {}
         res_set = set()
         for i, c in enumerate(word, 0):
-            if c in first_indices.keys() and c.isupper():
+            if c in indices.keys() and c.isupper():
                 continue
             else:
-                first_indices[c] = i
-
+                indices[c] = i
         for w in word:
-            if w.upper() in first_indices.keys() and w.lower() in first_indices.keys():
-                if first_indices[w.lower()] < first_indices[w.upper()]:
+            if w.upper() in indices.keys() and w.lower() in indices.keys():
+                if indices[w.lower()] < indices[w.upper()]:
                     res_set.add(w.lower())
         return len(res_set)
